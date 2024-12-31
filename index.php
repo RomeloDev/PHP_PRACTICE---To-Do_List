25482b4cbddb28033cf673acd4a385d53dcd6c91
RomeloDev <romelolucot1232@gmail.com>
2024-12-31 21:33:21 +0800

    Style added

--- index.php
+++ index.php
@@ -59,8 +59,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To do List</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="container">
     <h1>To-Do List</h1>
     <form action="index.php" method="post">
         <input type="text" name="task" id="task" placeholder="Enter your task" required>
@@ -99,5 +101,6 @@
             }
         ?>
     </table>
+    </div>
 </body>
 </html>
